0e3fbd70c0645bc39795e007501c59448ef68b93
franciniltonsoaresmenzes <fransoaresmenzes@gmail.com>
2023-06-08 21:47:03 -0300

    Primeiro caso de uso

---
+++ src/domain/use-cases/answer-question.ts
@@ -0,0 +1,15 @@
+import { Answer } from "../entities/answer"
+
+interface AnswerQuestionUseCaseResquest {
+  instructorId: string
+  questionId: string
+  content: string
+}
+
+export class AnswerQuestionUseCase {
+  execute({ instructorId, questionId, content }:AnswerQuestionUseCaseResquest) {
+    const answer = new Answer(content)
+
+    return answer
+  }
+}
