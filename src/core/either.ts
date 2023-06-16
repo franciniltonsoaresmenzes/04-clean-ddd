14351ce9e0e158fc1a476a9b6365a475139ac81f
franciniltonsoaresmenzes <fransoaresmenzes@gmail.com>
2023-06-16 19:59:42 -0300

    Testando classes de erro

--- src/core/either.ts
+++ src/core/either.ts
@@ -1,25 +1,41 @@
-export class Left<L> {
+export class Left<L, A> {
   readonly value: L
 
   constructor(value: L) {
     this.value = value
   }
+
+  isLeft(): this is Left<L, A> {
+    return true
+  }
+
+  isRight(): this is Right<L, A> {
+    return false
+  }
 }
+export class Right<L, A> {
+  readonly value: A
 
-export class Right<R> {
-  readonly value: R
-
-  constructor(value: R) {
+  constructor(value: A) {
     this.value = value
+  }
+
+  isLeft(): this is Left<L, A> {
+    return false
+  }
+
+  isRight(): this is Right<L, A> {
+    return true
   }
 }
 
-export type Either<L, R> = Left<L> | Right<R>
+export type Either<L, A> = Left<L, A> | Right<L, A>
 
-export const left = <L, R>(value: L): Either<L, R> => {
-  return new Left(value)
+export const left = <L, A>(l: L): Either<L, A> => {
+  return new Left<L, A>(l)
 }
 
-export const right = <L, R>(value: R): Either<L, R> => {
-  return new Right(value)
+export const right = <L, A>(a: A): Either<L, A> => {
+  return new Right<L, A>(a)
 }
+
