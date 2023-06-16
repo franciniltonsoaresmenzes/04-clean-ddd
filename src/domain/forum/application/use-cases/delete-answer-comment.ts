6805f8cf9154cf9ba03b0f6e376dfaeae5e74f6e
franciniltonsoaresmenzes <fransoaresmenzes@gmail.com>
2023-06-16 20:35:31 -0300

    Refatorando casos de uso

--- src/domain/forum/application/use-cases/delete-answer-comment.ts
+++ src/domain/forum/application/use-cases/delete-answer-comment.ts
@@ -1,12 +1,17 @@
 import { Either, left, right } from '@/core/either'
 import { AnswerCommentsRepository } from '../repositories/answer-comment-repository'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+import { NotAllowedError } from './errors/not-allowed-error'
 
 interface DeleteAnswerCommentUseCaseResquest {
   authorId: string
   answerCommentId: string
 }
 
-type DeleteAnswerCommentUseCaseResponse = Either<string, {}>
+type DeleteAnswerCommentUseCaseResponse = Either<
+  ResourceNotFoundError | NotAllowedError,
+  {}
+>
 
 export class DeleteAnswerCommentUseCase {
   constructor(private answerCommentRepository: AnswerCommentsRepository) {}
@@ -19,10 +24,10 @@ export class DeleteAnswerCommentUseCase {
       answerCommentId,
     )
 
-    if (!answerComment) return left('Answer comment not found.')
+    if (!answerComment) return left(new ResourceNotFoundError())
 
     if (authorId !== answerComment.authorId.toSring()) {
-      return left('Not allowed.')
+      return left(new NotAllowedError())
     }
 
     await this.answerCommentRepository.delete(answerComment)
